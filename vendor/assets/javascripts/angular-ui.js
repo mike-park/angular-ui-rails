53e4a91b244d3fccb9dfbd4b3e235d5c6878f7a9
Zethus Suen <zethus@stackpop.com>
2013-08-15 18:08:23 -0400

    update tinyMCE to support 4.x

--- vendor/assets/javascripts/angular-ui.js
+++ vendor/assets/javascripts/angular-ui.js
@@ -1196,53 +1196,74 @@ angular.module('ui.directives').directive('uiSortable', [
  */
 angular.module('ui.directives').directive('uiTinymce', ['ui.config', function (uiConfig) {
   uiConfig.tinymce = uiConfig.tinymce || {};
+  var generateIds = 0;
   return {
-    require: 'ngModel',
-    link: function (scope, elm, attrs, ngModel) {
-      var expression,
+      require: 'ngModel',
+      link: function (scope, elm, attrs, ngModel) {
+        var expression, options, tinyInstance,
+          updateView = function () {
+            ngModel.$setViewValue(elm.val());
+            if (!scope.$$phase) {
+              scope.$apply();
+            }
+          };
+        // generate an ID if not present
+        if (!attrs.id) {
+          attrs.$set('id', 'uiTinymce' + generatedIds++);
+        }
+
+        if (attrs.uiTinymce) {
+          expression = scope.$eval(attrs.uiTinymce);
+        } else {
+          expression = {};
+        }
         options = {
-          // Update model on button click
-          onchange_callback: function (inst) {
-            if (inst.isDirty()) {
-              inst.save();
-              ngModel.$setViewValue(elm.val());
-              if (!scope.$$phase)
-                scope.$apply();
-            }
-          },
-          // Update model on keypress
-          handle_event_callback: function (e) {
-            if (this.isDirty()) {
-              this.save();
-              ngModel.$setViewValue(elm.val());
-              if (!scope.$$phase)
-                scope.$apply();
-            }
-            return true; // Continue handling
-          },
           // Update model when calling setContent (such as from the source editor popup)
           setup: function (ed) {
-            ed.onSetContent.add(function (ed, o) {
-              if (ed.isDirty()) {
-                ed.save();
-                ngModel.$setViewValue(elm.val());
-                if (!scope.$$phase)
-                  scope.$apply();
-              }
+            var args;
+            ed.on('init', function(args) {
+              ngModel.$render();
             });
+            // Update model on button click
+            ed.on('ExecCommand', function (e) {
+              ed.save();
+              updateView();
+            });
+            // Update model on keypress
+            ed.on('KeyUp', function (e) {
+              ed.save();
+              updateView();
+            });
+            // Update model on change, i.e. copy/pasted text, plugins altering content
+            ed.on('SetContent', function (e) {
+              ed.save();
+              updateView();
+            });
+            if (expression.setup) {
+              scope.$eval(expression.setup);
+              delete expression.setup;
+            }
+          },
+          mode: 'exact',
+          elements: attrs.id
+        };
+        // extend options with initial uiTinymceConfig and options from directive attribute value
+        angular.extend(options, uiTinymceConfig, expression);
+        setTimeout(function () {
+          tinymce.init(options);
+        });
+
+
+        ngModel.$render = function() {
+          if (!tinyInstance) {
+            tinyInstance = tinymce.get(attrs.id);
+          }
+          if (tinyInstance) {
+            tinyInstance.setContent(ngModel.$viewValue || '');
           }
         };
-      if (attrs.uiTinymce) {
-        expression = scope.$eval(attrs.uiTinymce);
-      } else {
-        expression = {};
       }
-      angular.extend(options, uiConfig.tinymce, expression);
-      setTimeout(function () {
-        elm.tinymce(options);
-      });
-    }
-  };
+    };
 }]);
 
 /**
